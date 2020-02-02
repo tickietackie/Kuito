59a753097b47f92aa68e9fa3e12427533b9de9f6
Kai Kuklok <kaikuklok@Kais-MBP.fritz.box>
2020-02-02 21:54:29 +0100

    Changed only class component to functional component

--- js/screens/Authentication/AuthLoading.js
+++ js/screens/Authentication/AuthLoading.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect}  from 'react';
 import {
   ActivityIndicator,
   AsyncStorage,
@@ -7,29 +7,29 @@ import {
   View,
 } from 'react-native';
 
-class AuthLoadingScreen extends React.Component {
-  componentDidMount() {
-    this._bootstrapAsync();
-  }
-
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
+const AuthLoadingScreen = function AuthLoadingScreen(props) {
+ 
+      // Fetch the token from storage then navigate to our appropriate place
+  const _bootstrapAsync = async () => {
     const userToken = await AsyncStorage.getItem('userId');
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+    props.navigation.navigate(userToken ? 'App' : 'Auth');
   };
 
-  // Render any loading content that you like here
-  render() {
+  useEffect(() => { // code to run on component mount
+
+    _bootstrapAsync();
+
+}, []) //pass an empty array to call it just with the first call --> }, [])
+
     return (
       <View>
         <ActivityIndicator />
         <StatusBar barStyle="default" />
       </View>
     );
-  }
 }
 
 export default AuthLoadingScreen;
